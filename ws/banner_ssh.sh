03f0dda0b35fb40d0c0d31a95cc32195d53c30ba
CODE1-cpu <yehyemahisa@gmail.com>
2025-01-30 20:45:53 +0700

    Add files via upload

---
+++ ws/banner_ssh.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+cat > /etc/issue.net << END
+<font color="green">nikmatin Kang....</font>
+<p style="text-align: center;">
+<font color="cyan"><b><big><big>ULTRASONIC TECHNOLOGY</font></b></big></big></p>
+<p style="text-align: center;">
+    <span style="color: #7FFF00;">Support us:</span>
+    <span style="color: #EFEFEF;">WA: wa.me/6285931925073</span>
+    <span style="color: #EFEFEF;">TL: t.me/ultrasonic</span>
+</p>
+END

7f282ac37e6a0f2d2288de640b90f4626691af2f
CODE1-cpu <yehyemahisa@gmail.com>
2025-02-20 23:02:05 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -56,6 +56,7 @@ d1=$(date -d "$exp" +%s)
 d2=$(date -d "$today" +%s)
 certifacate=$(((d1 - d2) / 86400))
 
+}
 
   useexp=$(curl -s $url_izin | grep $IP | awk '{print $3}')
   if [[ $date_list < $useexp ]]; then
